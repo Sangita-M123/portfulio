068ffe72091072d280eac444f0948567ca1cb459
Sangita-M123 <153701193+Sangita-M123@users.noreply.github.com>
2025-02-07 14:25:42 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,79 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import java from "./tech/java.png";
+import expressjs from "./tech/expressjs.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import slashMark from "./company/slashMark.png";
+import eduonix from "./company/eduonix.png";
+// import starbucks from "./company/starbucks.png";
+// import tesla from "./company/tesla.png";
+
+import food from "./food.png";
+import expense from "./expense.png";
+import ichat from "./ichat.png";
+
+import cer1 from "./cer1.jpeg";
+import cer2 from "./cer2.jpg";
+import cer3 from "./cer3.jpg";
+import cer4 from "./cer4.jpg";
+import cer5 from "./cer5.jpg";
+import cer6 from "./cer6.jpg";
+
+import gfg from "./gfg.png";
+import hackerrank from "./hackerrank.png";
+import leetcode from "./leetcode.png";
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  java,
+  expressjs,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  eduonix,
+  slashMark,
+  food,
+  expense,
+  ichat,
+  cer1,
+  cer2,
+  cer3,
+  cer4,
+  cer5,
+  cer6,
+  gfg,
+  leetcode,
+  hackerrank,
+};
